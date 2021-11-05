e6968ce8efe61021b8bfbacb4e7349461e3d6322
Блажевич Дмитрий <dima1988_bl@mail.ru>
2021-11-05 22:26:06 +0300

    изм_5

--- Proj_02/Program.cs
+++ Proj_02/Program.cs
@@ -1,15 +1,47 @@
-﻿// 1. По двум заданным числам проверять является ли первое квадратом второго
+﻿// 0. Вывести квадрат числа 
+// 1. По двум заданным числам проверять является ли первое квадратом второго 
+// 2. Даны два числа. Показать большее и меньшее число 
+// 3. По заданному номеру дня недели вывести его название 
+// 4. Найти максимальное из трех чисел 
+// 5. Написать программу вычисления значения функции y=f(a)
 
+// 0. Вывести квадрат числа
 
-Console.WriteLine ("Введите первое число");
- int a =
-  Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine ("Введите число, квадрат которого хотите получить");
+ //int a =
+  //Convert.ToInt32(Console.ReadLine());
+//int b = a * a;
+//Console.WriteLine ($"Квадрат {a} = {b}");
 
-Console.WriteLine ("Введите второе число");
-int b = 
-  Convert.ToInt32(Console.ReadLine());
-  if(b*b==a)
-  Console.WriteLine ($" Число {a} является квадратом {b}");
+// 1. По двум заданным числам проверять является ли первое квадратом второго 
 
-else
-Console.WriteLine ($"Число {a} не является квадратом {b}");
+//Console.WriteLine ("Введите первое число");
+ //int a =
+  //Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine ("Введите второе число");
+//int b = 
+  //Convert.ToInt32(Console.ReadLine());
+  //if(b*b==a)
+  //Console.WriteLine ($" Число {a} является квадратом {b}");
+
+//else
+//Console.WriteLine ($"Число {a} не является квадратом {b}");
+
+// 2. Даны два числа. Показать большее и меньшее число 
+//Console.WriteLine ("Введите первое число");
+//int a =
+//Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine ("Введите второе число");
+//int b = 
+//Convert.ToInt32(Console.ReadLine());
+
+//if(a>b)
+// Console.WriteLine($"Большее число {a}");
+//Console.WriteLine();
+//if(a<b)
+//Console.WriteLine ($"Большее число {b}");
+//Console.WriteLine();
+//if(a==b)
+//Console.WriteLine("Числа одинаковые");
