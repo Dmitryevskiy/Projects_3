8a7c3a155e34cd90dc939ba9a6ca45fd2f0511eb
Блажевич Дмитрий <dima1988_bl@mail.ru>
2021-11-05 21:55:05 +0300

    изм_4

---
+++ Proj_02/Program.cs
@@ -0,0 +1,15 @@
+﻿// 1. По двум заданным числам проверять является ли первое квадратом второго
+
+
+Console.WriteLine ("Введите первое число");
+ int a =
+  Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число");
+int b = 
+  Convert.ToInt32(Console.ReadLine());
+  if(b*b==a)
+  Console.WriteLine ($" Число {a} является квадратом {b}");
+
+else
+Console.WriteLine ($"Число {a} не является квадратом {b}");
